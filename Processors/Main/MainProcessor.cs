671578a80304990ee1e0f37ff507915d56776fb7
Vladyan18 <Vladyan18@gmail.com>
2016-10-06 19:49:50 +0600

    Созданы обработчики

---
+++ Processors/Main/MainProcessor.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Processors.Main
+{
+    public class MainProcessor
+    {
+    }
+}
