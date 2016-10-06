a9cc1776511e85a6bad6f2a02cc4cffe9194b427
Vladyan18 <Vladyan18@gmail.com>
2016-10-06 21:49:09 +0600

    .

--- Processors/Main/MainProcessor.cs
+++ Processors/Main/MainProcessor.cs
@@ -9,12 +9,26 @@ namespace Processors.Main
 {
     public class MainProcessor
     {
-        private Dictionary<string, List<Order>> ordersBase = new Dictionary<string, List<Order>>();
+        private Dictionary<string, Queue<Order>> ordersBase = new Dictionary<string, Queue<Order>>();
         private List<IMinisteryProcessor> ministeryProcessors = new List<IMinisteryProcessor>();
+        private List<Order> currentOrdersLine = new List<Order>();
+
+        public MainProcessor()
+        {
+            ministeryProcessors.Add(new MinIndustryProcessor());
+            ministeryProcessors.Add(new MinFinanceProcessor());
+            ministeryProcessors.Add(new MinMilitaryProcessor());
+            ministeryProcessors.Add(new MinForeignProcessor());
+            ministeryProcessors.Add(new MinMediaProcessor());
+            ministeryProcessors.Add(new MinMVDProcessor());
+            ministeryProcessors.Add(new MinFSBProcessor());
+            ministeryProcessors.Add(new MinScienceProcessor());
+            ministeryProcessors.Add(new MinPremierProcessor());
+        }
 
         public void addCountry(string name)
         {
-            ordersBase.Add(name, new List<Order>());
+            ordersBase.Add(name, new Queue<Order>());
         }
 
         public void removeCountry(string name)
@@ -24,7 +38,27 @@ namespace Processors.Main
 
         public void addOrder(string name, Order newOrder)
         {
-            ordersBase[name].Add(newOrder);
+            ordersBase[name].Enqueue(newOrder);
         }
+
+        public void processOrders()
+        {
+            while(ordersBase.Any(x => x.Value.Any()))
+            {
+                foreach (KeyValuePair<string, Queue<Order>> qu in ordersBase.Where(x => x.Value.Any()))
+                {
+                    currentOrdersLine.Add(qu.Value.Dequeue());
+                }
+
+                // захуярить фсбшную обработку
+
+                for (int i = 0; i < currentOrdersLine.Count; i++)
+                {
+                    ministeryProcessors[currentOrdersLine[i].args[0]].processOrder(currentOrdersLine[i]);
+                }
+                currentOrdersLine.Clear();
+            }
+        }
+
     }
 }
