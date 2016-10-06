3f6f03c0fb392909a48517f35de0aa2da44c4f76
Vladyan18 <Vladyan18@gmail.com>
2016-10-06 22:40:51 +0600

    06.10.2016 22:40

---
+++ CommonClasses/NukeRocket.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommonClasses
+{
+    public class NukeRocket
+    {
+        string from;
+        string to;
+        public int lifeTime;
+        public int count;
+
+        public NukeRocket(string from, string to)
+        {
+            this.from = from;
+            this.to = to;
+            lifeTime = Constants.nukeRocketLifetime;
+        }
+    }
+}
--- Processors/Nuke/NukeProcessor.cs
+++ Processors/Nuke/NukeProcessor.cs
@@ -1,12 +1,44 @@
-﻿using System;
+﻿using CommonClasses;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Processors.Nuke
 {
     class NukeProcessor
     {
+        const int delay = 500;
+        private BackgroundWorker timer = new BackgroundWorker();
+        private List<NukeRocket> rockets = new List<NukeRocket>();
+
+        public NukeProcessor()
+        {
+            timer.DoWork += timer_work;
+            timer.WorkerReportsProgress = true;
+            timer.ProgressChanged += timer_tick;
+        }
+
+        private void timer_tick(object sender, ProgressChangedEventArgs e)
+        {
+
+        }
+
+        private void timer_work(object sender, DoWorkEventArgs e)
+        {
+            while (rockets.Any())
+            {
+                foreach(NukeRocket rckt in rockets)
+                {
+                    rckt.lifeTime -= delay;
+                }
+
+                timer.ReportProgress(0);            
+                Thread.Sleep(delay);
+            } 
+        }
     }
 }
